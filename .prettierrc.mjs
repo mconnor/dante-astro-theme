26d6527c24d097e5a8bf30bfa57223fb2880df52
mike connor <mike@mikeconnor.tech>
2024-10-22 10:17:43 -0500

    new prettier setup

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,16 +1,22 @@
-// .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ['prettier-plugin-astro'],
-  overrides: [
-    {
-      files: '*.astro',
-      options: {
-        parser: 'astro',
-      },
-    },
-  ],
-  singleQuote: true,
-  trailingComma: 'all',
-  tabWidth: 2,
+    plugins: ['prettier-plugin-astro'],
+    overrides: [
+        {
+            files: '*.astro',
+            options: {
+                parser: 'astro'
+            }
+        },
+        {
+            files: ['*.md', '*.mdx', '*.yaml'],
+            options: {
+                tabWidth: 2
+            }
+        }
+    ],
+    printWidth: 160,
+    singleQuote: true,
+    trailingComma: 'none',
+    tabWidth: 4
 };
